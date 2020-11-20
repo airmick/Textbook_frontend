3ddf6eb24500cb5ac519ca64f05d952f98c73311
JMichaelson <aelmick92@gmail.com>
2020-11-21 00:23:51 +0200

    modification in login and register form

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "GET":
+        return render_template('index.html')
+    if request.method == "POST":
+        return render_template('greet.html', name=request.form.get("name", "world"))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Log user in"""
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
